dff72ee00080324741472c73fc146531f05dec87
Gabriel Rojas <karpofv.89@gmail.com>
2017-07-27 19:44:44 -0400

    cambios en la solicitud de reparaciones

--- bd/fundainv.sql
+++ bd/fundainv.sql
@@ -31,9 +31,13 @@ CREATE TABLE IF NOT EXISTS `componente` (
   UNIQUE KEY `comp_biennac` (`comp_biennac`),
   KEY `FK_componente_tools_estatus` (`comp_estado`),
   CONSTRAINT `FK_componente_tools_estatus` FOREIGN KEY (`comp_estado`) REFERENCES `tools_estatus` (`est_codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin CHECKSUM=1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin CHECKSUM=1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.componente: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `componente` DISABLE KEYS */;
+INSERT INTO `componente` (`comp_codigo`, `comp_fechain`, `comp_nombre`, `comp_descripcion`, `comp_marca`, `comp_modelo`, `comp_serial`, `comp_biennac`, `comp_estado`) VALUES
+	(5, '2017-07-08 18:22:46', 'a165as1d65', '5a1sd65as16', '5s1a6d51a', '651s65d1a6', '651', 'dfsd1f65', 3);
+/*!40000 ALTER TABLE `componente` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.movimientos
@@ -52,9 +56,13 @@ CREATE TABLE IF NOT EXISTS `movimientos` (
   PRIMARY KEY (`mov_codigo`),
   KEY `FK_movimientos_componente` (`mov_compcodigo`),
   CONSTRAINT `FK_movimientos_componente` FOREIGN KEY (`mov_compcodigo`) REFERENCES `componente` (`comp_codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.movimientos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `movimientos` DISABLE KEYS */;
+INSERT INTO `movimientos` (`mov_codigo`, `mov_fecha`, `mov_compcodigo`, `mov_solicitante`, `mov_solresp`, `mov_motivo`, `mov_fechadev`, `mov_respdep`, `mov_perresp`, `mov_entrada`, `mov_salida`) VALUES
+	(1, '2017-07-11', 5, 'saasdasd', 'asda', 'sdasd', '2017-07-08', 'asdasdasd', 'asdasd', '2017-07-06', '2017-07-07');
+/*!40000 ALTER TABLE `movimientos` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.m_menu_emp_menj
@@ -73,9 +81,14 @@ CREATE TABLE IF NOT EXISTS `m_menu_emp_menj` (
   `CAdmin` char(2) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `orden` (`orden`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.m_menu_emp_menj: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `m_menu_emp_menj` DISABLE KEYS */;
+INSERT INTO `m_menu_emp_menj` (`id`, `ConexMenuMaster`, `orden`, `menu`, `conex`, `funcion`, `Imagen`, `ancho`, `alto`, `nivel`, `CA`, `CAdmin`) VALUES
+	(54, NULL, NULL, 'Administrador', 'menu.php', NULL, '', NULL, NULL, NULL, NULL, NULL),
+	(77, NULL, NULL, 'Equipos', NULL, NULL, '', NULL, NULL, NULL, NULL, NULL);
+/*!40000 ALTER TABLE `m_menu_emp_menj` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.m_menu_emp_sub_menj
@@ -112,9 +125,19 @@ CREATE TABLE IF NOT EXISTS `m_menu_emp_sub_menj` (
   PRIMARY KEY (`id`),
   KEY `enlace` (`enlace`),
   CONSTRAINT `m_menu_emp_sub_menj_ibfk_1` FOREIGN KEY (`enlace`) REFERENCES `m_menu_emp_menj` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=169 DEFAULT CHARSET=utf8;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.m_menu_emp_sub_menj: ~7 rows (aproximadamente)
+/*!40000 ALTER TABLE `m_menu_emp_sub_menj` DISABLE KEYS */;
+INSERT INTO `m_menu_emp_sub_menj` (`id`, `enlace`, `enlacesub`, `Act`, `orden`, `menu`, `conex`, `Url_1`, `Url_2`, `Url_3`, `Url_4`, `Url_5`, `Url_6`, `Url_7`, `Url_8`, `Url_9`, `Url_10`, `Inserte`, `Updated`, `Deleted`, `Acciones`, `Ejecutar`, `conexd`, `funcion`, `nivel`, `CA`, `CAdmin`, `CssColor`, `CssImagen`) VALUES
+	(55, 54, NULL, NULL, NULL, 'Asignar Usuarios', 'menu.php', 'conf_usuario/crear_usuario.php', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', ''),
+	(110, 54, NULL, NULL, NULL, 'Administrar Perfiles', 'menu.php', 'admin_perfil/conf_perfil.php', 'admin_perfil/conf_menu_list.php', 'admin_perfil/conf_menu_accion.php', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', ''),
+	(161, 77, NULL, NULL, 1, 'Registrar equipo', NULL, 'sistema/equipo/equipo.php', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', ''),
+	(163, 77, NULL, NULL, 2, 'Buscar equipo', NULL, 'sistema/equipo/buscar.php', 'sistema/equipo/reparar.php', 'sistema/equipo/movimiento.php', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', ''),
+	(164, 77, NULL, NULL, 4, 'Reparaciones', NULL, 'sistema/solicitar/reparar.php', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', ''),
+	(166, 77, NULL, NULL, 3, 'Movimientos', NULL, 'sistema/movimiento/movimiento.php', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', ''),
+	(168, 77, NULL, NULL, NULL, 'Buscar equipo', NULL, 'sistema/equipo/buscar2.php', 'sistema/equipo/reparar.php', '', '', '', NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', NULL, NULL, NULL, NULL, NULL, '', '');
+/*!40000 ALTER TABLE `m_menu_emp_sub_menj` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.perfiles
@@ -123,9 +146,15 @@ CREATE TABLE IF NOT EXISTS `perfiles` (
   `Nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`CodPerfil`),
   UNIQUE KEY `Nombre` (`Nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.perfiles: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `perfiles` DISABLE KEYS */;
+INSERT INTO `perfiles` (`CodPerfil`, `Nombre`) VALUES
+	(1, 'Administrador'),
+	(2, 'god'),
+	(3, 'operador');
+/*!40000 ALTER TABLE `perfiles` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.perfiles_det
@@ -147,9 +176,29 @@ CREATE TABLE IF NOT EXISTS `perfiles_det` (
   CONSTRAINT `FK_perfiles_det_perfiles` FOREIGN KEY (`IdPerfil`) REFERENCES `perfiles` (`CodPerfil`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `perfiles_det_ibfk_2` FOREIGN KEY (`Menu`) REFERENCES `m_menu_emp_menj` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `perfiles_det_ibfk_3` FOREIGN KEY (`Submenu`) REFERENCES `m_menu_emp_sub_menj` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=290 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.perfiles_det: ~17 rows (aproximadamente)
+/*!40000 ALTER TABLE `perfiles_det` DISABLE KEYS */;
+INSERT INTO `perfiles_det` (`id`, `IdPerfil`, `Submenu`, `Menu`, `S`, `U`, `D`, `I`, `P`) VALUES
+	(1, 1, 110, 54, 0, 1, 1, 1, 1),
+	(113, 1, 55, 54, 1, 1, 1, 1, 1),
+	(225, 2, 110, 54, 1, 1, 1, 1, 1),
+	(226, 2, 55, 54, 1, 1, 1, 1, 1),
+	(236, 2, 163, 77, 1, 1, 1, 1, 1),
+	(237, 2, 166, 77, 1, 1, 1, 1, 1),
+	(238, 2, 164, 77, 1, 1, 1, 1, 1),
+	(239, 2, 161, 77, 1, 1, 1, 1, 1),
+	(256, 1, 163, 77, 1, 1, 1, 1, 1),
+	(257, 1, 166, 77, 1, 1, 1, 1, 1),
+	(258, 1, 161, 77, 1, 1, 1, 1, 1),
+	(259, 1, 164, 77, 1, 1, 1, 1, 1),
+	(276, 3, 163, 77, 0, 0, 0, 0, 0),
+	(277, 3, 166, 77, 0, 0, 0, 0, 0),
+	(278, 3, 161, 77, 0, 0, 0, 0, 0),
+	(279, 3, 164, 77, 0, 0, 0, 0, 0),
+	(288, 3, 168, 77, 1, 0, 0, 1, 0);
+/*!40000 ALTER TABLE `perfiles_det` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.recargar
@@ -159,9 +208,20 @@ CREATE TABLE IF NOT EXISTS `recargar` (
   `actd` int(1) NOT NULL,
   `Accion` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=354 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.recargar: ~8 rows (aproximadamente)
+/*!40000 ALTER TABLE `recargar` DISABLE KEYS */;
+INSERT INTO `recargar` (`id`, `URL`, `actd`, `Accion`) VALUES
+	(1, 'uploader/receiver.php', 0, ''),
+	(2, 'recargar/recargar.php', 0, ''),
+	(3, 'recargar/recargar.php', 0, ''),
+	(4, 'sistema/documentos/selectorAnual.php', 0, ''),
+	(5, 'sistema/documentos/selectorMes.php', 0, ''),
+	(351, 'sistema/index.php', 0, ''),
+	(352, 'recargar/recargar.php', 1, ''),
+	(353, 'sistema/reportes/pdf_constancia.php', 0, '');
+/*!40000 ALTER TABLE `recargar` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.registrados
@@ -177,9 +237,15 @@ CREATE TABLE IF NOT EXISTS `registrados` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `cedula` (`cedula`),
   CONSTRAINT `FK_registrados_usuarios` FOREIGN KEY (`cedula`) REFERENCES `usuarios` (`Cedula`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.registrados: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `registrados` DISABLE KEYS */;
+INSERT INTO `registrados` (`id`, `nacionalidad`, `Usuario`, `cedula`, `Nombres`, `Apellidos`, `sexo`, `correo`) VALUES
+	(5, '', 0, 12345, 'GOD', 'GOD', '', ''),
+	(6, '', 0, 123123, 'admin', 'admin', '', ''),
+	(7, '', 0, 123321, 'operadores', 'operadores', '', '');
+/*!40000 ALTER TABLE `registrados` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.reparacion
@@ -194,9 +260,13 @@ CREATE TABLE IF NOT EXISTS `reparacion` (
   PRIMARY KEY (`rep_codigo`),
   KEY `FK_reparacion_componente` (`rep_equipo`),
   CONSTRAINT `FK_reparacion_componente` FOREIGN KEY (`rep_equipo`) REFERENCES `componente` (`comp_codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.reparacion: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `reparacion` DISABLE KEYS */;
+INSERT INTO `reparacion` (`rep_codigo`, `rep_fecha`, `rep_equipo`, `rep_motivo`, `rep_respuesta`, `rep_fecresp`, `rep_status`) VALUES
+	(1, '2017-07-11 20:03:36', 5, 'SE DAÑO', NULL, NULL, 3);
+/*!40000 ALTER TABLE `reparacion` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.sexo
@@ -204,9 +274,14 @@ CREATE TABLE IF NOT EXISTS `sexo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Sexo';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Sexo';
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.sexo: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `sexo` DISABLE KEYS */;
+INSERT INTO `sexo` (`id`, `Nombre`) VALUES
+	(1, 'Masculino'),
+	(2, 'Femenino');
+/*!40000 ALTER TABLE `sexo` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.tools_estatus
@@ -215,9 +290,16 @@ CREATE TABLE IF NOT EXISTS `tools_estatus` (
   `est_descripcion` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`est_codigo`),
   UNIQUE KEY `est_descripcion` (`est_descripcion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.tools_estatus: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `tools_estatus` DISABLE KEYS */;
+INSERT INTO `tools_estatus` (`est_codigo`, `est_descripcion`) VALUES
+	(2, 'ACTIVO'),
+	(4, 'DAÑADO'),
+	(3, 'EN MANTENIMIENTO'),
+	(1, 'INACTIVO');
+/*!40000 ALTER TABLE `tools_estatus` ENABLE KEYS */;
 
 
 -- Volcando estructura para tabla fundainv.usuarios
@@ -239,9 +321,16 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   KEY `Cedula` (`Codigo`,`Usuario`,`Cedula`),
   KEY `Nivel` (`Nivel`),
   CONSTRAINT `FK_usuarios_perfiles` FOREIGN KEY (`Nivel`) REFERENCES `perfiles` (`CodPerfil`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci CHECKSUM=1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci CHECKSUM=1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla fundainv.usuarios: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` (`id`, `Cedula`, `Usuario`, `contrasena`, `Tipo`, `Nivel`, `Codigo`, `Registro`, `Fecha`, `Observacion`) VALUES
+	(1, 123321, 'operador', '06d4f07c943a4da1c8bfe591abbc3579', 'Empleado', 3, 'bb1e6', NULL, '0000-00-00 00:00:00', NULL),
+	(2, 123123, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Empleado', 1, '34d38', NULL, '0000-00-00 00:00:00', NULL),
+	(3, 12345, 'GOD', 'a1b995eb2627f17bfd5fcb1de8533c62', 'Empleado', 2, '2c48a', NULL, '0000-00-00 00:00:00', NULL),
+	(4, 12345, '', '', '', NULL, '2c48a', NULL, '0000-00-00 00:00:00', NULL);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- system/sistema/equipo/buscar2.php
+++ system/sistema/equipo/buscar2.php
@@ -2,12 +2,41 @@
 <script type="text/javascript" src="<?php echo $ruta_base; ?>assets/plugins/datatables/jquery-1.12.4.js"></script>
 <script type="text/javascript" src="<?php echo $ruta_base; ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
 <?php
-    $codigo = $_POST[codigo];
+$codigo = $_POST[codigo];
+$motivo = $_POST[motivo];
+$codigorep = $_POST[codigorep];
+$respuesta = $_POST[respuesta];
+$estado = $_POST[estado];
+$editar = $_POST[editar];
+$eliminar = $_POST[eliminar];
+if($editar==1 and $motivo!="" and $codigorep==""){
+    $insertar = paraTodos::arrayInserte("rep_equipo, rep_motivo,rep_status", "reparacion", "$codigo, '$motivo',3");
+    if($insertar){
+        $update = paraTodos::arrayUpdate("comp_estado=3", "componente", "comp_codigo=$codigo");
+    }
+}
+if($editar==1 and $respuesta!=""){ 
+    $update = paraTodos::arrayUpdate("rep_respuesta='$respuesta', rep_fecresp=current_date, rep_status='$estado'", "reparacion", "rep_codigo=$codigorep");
+    $codigorep="";
+}
+if($eliminar==1){
+    $delete = paraTodos::arrayDelete("rep_codigo=$codigorep", "reparacion");
+    $codigorep="";
+}
+$consulsol = paraTodos::arrayConsulta("*", "componente", "comp_codigo=$codigo");
+foreach($consulsol as $row){
+    $numero = $row[comp_biennac];
+    $serial = $row[comp_serial];
+    $nombre = $row[comp_nombre];
+    $desdripcion = $row[comp_descripcion];
+    $marca = $row[comp_marca];
+    $modelo = $row[comp_modelo];
+}
 ?>
 <div class="content">
     <div>
         <div class="page-header-title">
-            <h4 class="page-title">Equipos</h4>
+            <h4 class="page-title">Equipos a reparar</h4>
         </div>
     </div>
     <div class="page-content-wrapper ">
@@ -16,57 +45,121 @@
                 <div class="col-md-12">
                     <div class="panel panel-primary">
                         <div class="panel-body">
-                            <h4 class="m-b-30 m-t-0">Buscar equipos</h4>
+                            <h4 class="m-b-30 m-t-0">Registro de equipos</h4>
                             <div class="row">
-                                <table class="table table-hover" id="usuarios">
-                                    <thead>
-                                        <tr>
-                                            <td class="text-center"><strong>Nº de Inv.</strong></td>
-                                            <td class="text-center"><strong>Serial</strong></td>
-                                            <td class="text-center"><strong>Nombre</strong></td>
-                                            <td class="text-center"><strong>Descripción</strong></td>
-                                            <td class="text-center"><strong>Marca</strong></td>
-                                            <td class="text-center"><strong>Modelo</strong></td>
-                                            <td class="text-center"><strong>Sol. Reparación</strong></td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $consulsol = paraTodos::arrayConsulta("*", "componente", "1=1");
+                    <div class="row">                       
+                        <div class="col-xs-6">
+                            <label><strong>Nº de Inventario:</strong></label>
+                            <p>
+                                <?php echo $numero;?>
+                            </p>
+                            <label><strong>Serial:</strong></label>
+                            <p>
+                                <?php echo $serial;?>
+                            </p>
+                            <label><strong>Nombre:</strong></label>
+                            <p>
+                                <?php echo $nombre;?>
+                            </p>
+                        </div>
+                        <div class="col-xs-6">
+                            <label><strong>Descripción:</strong></label>
+                            <p>
+                                <?php echo $descripcion;?>
+                            </p>
+                            <label><strong>Marca:</strong></label>
+                            <p>
+                                <?php echo $marca;?>
+                            </p>
+                            <label><strong>Modelo:</strong></label>
+                            <p>
+                                <?php echo $modelo;?>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <label class="control-label">Respuesta</label>
+                            <input class="form-control" id="respuesta">
+                        </div>
+                        <div class="col-sm-4">
+                            <label class="control-label" for="estado">Estatus</label>
+                            <select id="estado" class="form-control">
+                                <?php
+                        combos::CombosSelect("1", "$estado", "*", "tools_estatus", "est_codigo", "est_descripcion", "1=1");
+                                ?>
+                            </select>
+                        </div>                
+                        <div class="col-sm-2">
+                            <br>
+                            <button class="btn btn-success" type="button" 
+                                    onclick="$.ajax({
+                                             url:'accion.php',
+                                             type:'POST',
+                                             data:{
+                                             dmn 	: <?php echo $idMenut;?>,
+                                             codigo 	: <?php echo $codigo;?>,
+                                             respuesta 	: $('#respuesta').val(),
+                                             codigorep 	: <?php echo $codigorep;?>,
+                                             estado 	: $('#estado').val(),
+                                             editar 	: 1,
+                                             ver 	: 2
+                                             },
+                                             success : function (html) {
+                                             $('#page-content').html(html);
+                                             },
+                                             });">Responder</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="custom-width-modalLabel">Reparaciones realizadas</h4>
+                        </div>
+                        <div class="divider">
+                            <table class="table table-hover" id="usuarios">
+                                <thead>
+                                    <tr>
+                                        <td class="text-center"><strong>Fecha de Solicitud</strong></td>
+                                        <td class="text-center"><strong>Motivo</strong></td>
+                                        <td class="text-center"><strong>Fecha de respuesta</strong></td>
+                                        <td class="text-center"><strong>Respuesta</strong></td>
+                                        <td class="text-center"><strong>Estado</strong></td>
+                                        <td class="text-center"><strong>Responder</strong></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                        $consulsol = paraTodos::arrayConsulta("*", "reparacion r, tools_estatus t", "r.rep_status=t.est_codigo");
                                         foreach($consulsol as $row){
                                         ?>
                                         <tr>
                                             <td class="text-center">
-                                                <?php echo $row[comp_biennac];?>
+                                                <?php echo paraTodos::convertDate($row[rep_fecha]);?>
                                             </td>
                                             <td class="text-center">
-                                                <?php echo utf8_decode($row[comp_serial]);?>
+                                                <?php echo utf8_decode($row[rep_motivo]);?>
                                             </td>
                                             <td class="text-center">
-                                                <?php echo utf8_decode($row[comp_nombre]);?>
+                                                <?php echo paraTodos::convertDate($row[rep_fecresp]);?>
                                             </td>
                                             <td class="text-center">
-                                                <?php echo utf8_decode($row[comp_descripcion]);?>
+                                                <?php echo utf8_decode($row[rep_respuesta]);?>
                                             </td>
                                             <td class="text-center">
-                                                <?php echo utf8_decode($row[comp_marca]);?>
+                                                <?php echo utf8_decode($row[est_descripcion]);?>
                                             </td>
-                                            <td class="text-center">
-                                                <?php echo utf8_decode($row[comp_modelo]);?>
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="javascript:void(0);"
-                                                   onclick="$.ajax({
+                                            <td class="text-center">                       
+                                                <a href="javascript:void(0);" onclick="$.ajax({
                                                             url:'accion.php',
                                                             type:'POST',
                                                             data:{
                                                             dmn 	: <?php echo $idMenut;?>,
-                                                            codigo 	: <?php echo $row[comp_codigo];?>,
-                                                            ver 	: 2,
-                                                            act     : 2
+                                                            codigo 	: <?php echo $row[rep_equipo];?>,
+                                                            codigorep 	: <?php echo $row[rep_codigo];?>,
+                                                            ver 	: 2
                                                             },
                                                             success : function (html) {
-                                                            $('#ventanaVer').html(html);
+                                                            $('#page-content').html(html);
                                                             },
                                                             }); return false;" style="color:green;"><i class="fa fa-share" aria-hidden="true"></i>
                                                 </a>
@@ -75,16 +168,14 @@
                                         <?php
                                         }
                                         ?>
-                                    </tbody>
-                                </table>
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 <script>
     $('#usuarios').DataTable({
         "language": {
